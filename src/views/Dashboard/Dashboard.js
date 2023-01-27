f47cf716930807d1f9c2b1871cd5689be163cfd0
tushar pandey <tushar0471@gmail.com>
2023-01-28 01:40:58 +0530

    Imported and Exported React

---
+++ src/views/Dashboard/Dashboard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <>
+      <div className="dashboard">
+        <h2>Dashboard Created</h2>
+        <div className="bomb-summary">
+          <div className="head">
+            <h2>Bomb Finance Summary</h2>
+          </div>
+          
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
